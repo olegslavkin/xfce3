b0cdc0e562095fbea2ee6a9812dbef59f16ed300
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2001-10-01 20:46:02 +0000

    First attempt to implement "led" style in clock display
    (Old svn revision: 197)

--- xfwm/events.c
+++ xfwm/events.c
@@ -1463,7 +1463,7 @@ HandleConfigureRequest ()
     height = cre->height + Tmp_win->title_height + 2 * (Tmp_win->boundary_width + Tmp_win->bw);
 
   /* Remove the MAXIMIZED flag */
-  tmp_win->flags &= ~MAXIMIZED;
+  Tmp_win->flags &= ~MAXIMIZED;
 
   SetupFrame (Tmp_win, x, y, width, height, False, True);
 #ifdef DEBUG
