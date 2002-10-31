dfb8f22e8151e318950552011c9c548424a7c7ac
Edscott Wilson <edscott@imp.mx>
2002-10-31 13:03:53 +0000

    - update version
    (Old svn revision: 1163)

--- xfsamba/xfsamba.h
+++ xfsamba/xfsamba.h
@@ -9,7 +9,7 @@
 #include "../xfdiff/xfdiff_colorsel.h"
 #include <sys/types.h>
 #ifndef XFSAMBA_VERSION
-#define XFSAMBA_VERSION "0.48"
+#define XFSAMBA_VERSION "0.49"
 #endif
 
 #ifdef XFSAMBA_MAIN
